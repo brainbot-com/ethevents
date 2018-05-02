c3fd2f616515beba1b31fb81783f0c7d62a1b2e9
Ralf Schmitt <ralf@systemexit.de>
2018-05-02 15:20:15 +0200

    make package installable
    setup.py declared packages to be an empty list. The result was that ethevents
    could only be installed in editable mode (pip install -e ...).
    
    Now we use setuptools.find_packages to find the list of packages.

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as requirements:
     reqs = requirements.read().split('\n')
@@ -13,7 +10,7 @@ with open('requirements-dev.txt') as requirements_dev:
 reqs = [req for req in reqs if req and req[0] != '#']
 
 config = {
-    'packages': [],
+    'packages': find_packages(),
     'scripts': [],
     'name': 'ethevents',
     'entry_points': {
